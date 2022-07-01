d030a866f09691d5388ff854ce4bbece14911337
Paul Gottinger <pgottinger@outlook.com>
2022-07-01 00:55:59 -0500

    inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./Components/TopButtons"
+import Inputs from "./Components/Inputs"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
     </div>
   );
 }
